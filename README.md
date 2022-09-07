281fe1de6b6d0497ccc1950a77921df9fdf90021
m_daniyal <danik@magomedow@mail.ru>
2022-09-07 18:12:34 +0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Первое приложение на React
-https://daniiyal.github.io/firstReact/
---
+++ precache-manifest.9d0a85215966cd7efb0a8d2998d511b7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f56989c7fcac088cc600",
+    "url": "/firstReact/static/css/main.286a59bb.chunk.css"
+  },
+  {
+    "revision": "f56989c7fcac088cc600",
+    "url": "/firstReact/static/js/main.f56989c7.chunk.js"
+  },
+  {
+    "revision": "f75fce5f6d2b0434bf49",
+    "url": "/firstReact/static/js/1.f75fce5f.chunk.js"
+  },
+  {
+    "revision": "9bacffae409a0199f0a8",
+    "url": "/firstReact/static/js/runtime~main.9bacffae.js"
+  },
+  {
+    "revision": "aa67d41aeaf7cd8e8ae76c7255ef4a33",
+    "url": "/firstReact/static/media/denzel.aa67d41a.jpg"
+  },
+  {
+    "revision": "059c6374f8ee5db20dba2afc99c1eebb",
+    "url": "/firstReact/static/media/bkg.059c6374.jpg"
+  },
+  {
+    "revision": "05e8d714c22f4c26358c0a233bdb8ad4",
+    "url": "/firstReact/static/media/957d913f3328a6a260ebdf996a90dd6a.05e8d714.jpg"
+  },
+  {
+    "revision": "47c25401e2243d24f59bccc58557f0f4",
+    "url": "/firstReact/static/media/unity.47c25401.gif"
+  },
+  {
+    "revision": "94a39310269f9861e75d38e2c140bd6b",
+    "url": "/firstReact/static/media/defisheye.94a39310.png"
+  },
+  {
+    "revision": "1696f0b989088aff136f17d073c8acea",
+    "url": "/firstReact/static/media/films.1696f0b9.png"
+  },
+  {
+    "revision": "05b02231f1dfd235dd5f3c99680754fb",
+    "url": "/firstReact/static/media/vk.05b02231.png"
+  },
+  {
+    "revision": "6a9fb2ed267819dd5076b8865f849eeb",
+    "url": "/firstReact/static/media/instagram.6a9fb2ed.png"
+  },
+  {
+    "revision": "3c6f16b1b5ae5c5bbfce75da9f595d54",
+    "url": "/firstReact/static/media/telegram.3c6f16b1.png"
+  },
+  {
+    "revision": "10bb41226a0cfbe2abf7f3efcb280996",
+    "url": "/firstReact/index.html"
+  }
+];
